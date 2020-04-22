8fe2e9c5b0cfbad604dd717611cf148c967b258a
Rakshit <rak1729e@gmail.com>
2020-04-22 23:34:06 +0530

    conditional outputs - 2

--- react-learn/my-app/src/Ninjas.js
+++ react-learn/my-app/src/Ninjas.js
@@ -2,16 +2,20 @@ import React from "react";
 
 const Ninjas = ({ ninjas }) => {
   //    console.log(this.props);
-  const ninjaList = ninjas.map((ninja) => {
-    return ninja.age > 20 ? (
-      <div className="ninja" key={ninja.id}>
-        <div>Name :{ninja.name}</div>
-        <div>Age : {ninja.age}</div>
-        <div>Belt : {ninja.belt}</div>
-      </div>
-    ) : null;
-  });
-  return <div className="ninja-list">{ninjaList}</div>;
+
+  return (
+    <div className="ninja-list">
+      {ninjas.map((ninja) => {
+        return ninja.age > 20 ? (
+          <div className="ninja" key={ninja.id}>
+            <div>Name :{ninja.name}</div>
+            <div>Age : {ninja.age}</div>
+            <div>Belt : {ninja.belt}</div>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Ninjas;
