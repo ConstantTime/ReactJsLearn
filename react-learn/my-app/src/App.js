fcb7a5eb3a757463c16ed4550d7e12240331a86e
Rakshit <rak1729e@gmail.com>
2020-04-22 23:31:52 +0530

    conditional outputs

--- react-learn/my-app/src/App.js
+++ react-learn/my-app/src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 // import "./App.css";
 import Ninjas from "./Ninjas";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> My first react app</h1>
-        <p> Welcome </p>
-      </header>
-      <Ninjas name="Ryu" age="25" belt="black" />
-      <Ninjas name="Ryu" age="30" belt="green" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Amana", age: 20, belt: "black", id: 2 },
+      { name: "Ram", age: 25, belt: "black", id: 3 },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> My first react app</h1>
+          <p> Welcome </p>
+        </header>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-learn/my-app/src/Ninjas.js
+++ react-learn/my-app/src/Ninjas.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render() {
-    //    console.log(this.props);
-    const { name, age, belt } = this.props;
-    return (
-      <div className="ninja">
-        <div>Name :{name}</div>
-        <div>Age : {age}</div>
-        <div>Belt : {belt}</div>
+const Ninjas = ({ ninjas }) => {
+  //    console.log(this.props);
+  const ninjaList = ninjas.map((ninja) => {
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name :{ninja.name}</div>
+        <div>Age : {ninja.age}</div>
+        <div>Belt : {ninja.belt}</div>
       </div>
-    );
-  }
-}
+    ) : null;
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
