1d42b2aad82578cb6f52372dba8dc1af9a7ac747
Rakshit <rak1729e@gmail.com>
2020-04-22 22:44:07 +0530

    props theory

--- react-learn/my-app/src/App.js
+++ react-learn/my-app/src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+import Ninjas from "./Ninjas";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> My first react app</h1>
+        <p> Welcome </p>
       </header>
+      <Ninjas name="Ryu" age="25" belt="black" />
+      <Ninjas name="Ryu" age="30" belt="green" />
     </div>
   );
 }
---
+++ react-learn/my-app/src/Ninjas.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Ninjas extends Component {
+  render() {
+    //    console.log(this.props);
+    const { name, age, belt } = this.props;
+    return (
+      <div className="ninja">
+        <div>Name :{name}</div>
+        <div>Age : {age}</div>
+        <div>Belt : {belt}</div>
+      </div>
+    );
+  }
+}
+
+export default Ninjas;
