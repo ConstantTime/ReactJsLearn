52be5eed43d4ec6fee083bbdfb927c5114fa6813
Rakshit <rak1729e@gmail.com>
2020-04-24 01:22:01 +0530

    todo-app logic

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" },
+    ],
+  };
 
-function App() {
-  return <div className="App"></div>;
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos: todos,
+    });
+  };
+
+  addTodo = (todo) => {
+    todo.id = Math.random() * 10;
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos,
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos deleteTodo={this.deleteTodo} todos={this.state.todos} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
